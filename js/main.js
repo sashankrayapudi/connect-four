1b1fd47dd7f5fe0c5ac6d2e5bd606460777253cf
sashankrayapudi <sashank.rayapudi@gmail.com>
2022-07-05 15:59:12 -0700

    update vertical/horizontal win logic

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ init();
 
 function init() {
     board = [
-        [1,0,0,0,0,-1], //col 0
+        [1,0,0,0,0,0], //col 0
         [0,0,0,0,0,0],
         [0,0,0,0,0,0],
         [0,0,0,0,0,0],
@@ -40,7 +40,7 @@ function init() {
     gameStatus = null;
 
     render();
-}
+};
 
 
 function render () {
@@ -51,14 +51,14 @@ function render () {
         });
     });
     renderMarkers();
-}
+};
 
 
 function renderMarkers() {
     markerEls.forEach(function(markerEl, colIdx) {
         markerEl.style.visibility = board[colIdx].includes(0) ? 'visible' : 'hidden';
     })
-}
+};
 
 // update all impacted state, then call render
 function handleDrop(evt) {
@@ -70,17 +70,24 @@ function handleDrop(evt) {
     colArr[rowIdx] = turn;
     gameStatus = getGameStatus();
     turn *= -1;
-
     render()
-}
+};
 
 function getGameStatus() {
 
     // check horizontal
     for (let c = 0; c < BOARD_HEIGHT; c++) {
         for (let r = 0; r < BOARD_WIDTH - 3; r++) {
-            if (board[c][r] === turn && board[c][r+1] === turn && board[c][r+2] === turn && board[c][r+3] === turn) return true;
+            if (board[r][c] === turn && board[r+1][c] === turn && board[r+2][c] === turn && board[r+3][c] === turn) return turn;
         }
     }
 
-}
+    // check vertical
+    for (let r = 0; r < BOARD_HEIGHT; r++) {
+        for (let c = 0; c < BOARD_WIDTH - 3; c++) {
+            if (board[r][c] === turn && board[r][c+1] === turn && board[r][c+2] === turn && board[r][c+3] === turn) return turn;
+        }
+    }
+
+    return null;
+};
